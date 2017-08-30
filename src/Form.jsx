c07f27331a8a32dc30d5096d492b4942cdd2579f
Nicolas Thouvenin <nthouvenin@gmail.com>
2017-08-30 09:34:41 +0200

    :lint:

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,46 +1,44 @@
 import React from 'react';
 
-export default class Form extends React.Component {
-    render() {
-        return (
-          <form action="https://api.istex.fr/document/" method="get" className="form-horizontal">
+function Form() {
+    return (
+        <form action="https://api.istex.fr/document/" method="get" className="form-horizontal">
             <div className="form-group">
-              <label htmlFor="q" className="col-sm-1 control-label">Requête</label>
-              <div className="col-sm-11">
-                <textarea
-                  name="q" id="q" rows="3" autoFocus="true" className="form-control"
-                  placeholder="brain surgery"
-                />
-              </div>
+                <label htmlFor="q" className="col-sm-1 control-label">Requête</label>
+                <div className="col-sm-11">
+                    <textarea
+                        name="q" id="q" rows="3" autoFocus="true" className="form-control"
+                        placeholder="brain surgery"
+                    />
+                </div>
             </div>
             <div className="checkbox">
-              <label htmlFor="extractMetadata" className="col-sm-offset-1">Métadonnées</label>
-              <input type="checkbox" name="extract" id="extractMetadata" value="metadata" />
+                <label htmlFor="extractMetadata" className="col-sm-offset-1">Métadonnées</label>
+                <input type="checkbox" name="extract" id="extractMetadata" value="metadata" />
             </div>
             <div className="checkbox">
-              <label htmlFor="extractFulltext" className="col-sm-offset-1">Fulltext</label>
-              <input type="checkbox" name="extract" id="extractFulltext" value="fulltext" />
+                <label htmlFor="extractFulltext" className="col-sm-offset-1">Fulltext</label>
+                <input type="checkbox" name="extract" id="extractFulltext" value="fulltext" />
             </div>
             <div className="checkbox">
-              <label htmlFor="extractEnrichments" className="col-sm-offset-1">Enrichments</label>
-              <input type="checkbox" name="extract" id="extractEnrichments" value="enrichments" />
+                <label htmlFor="extractEnrichments" className="col-sm-offset-1">Enrichments</label>
+                <input type="checkbox" name="extract" id="extractEnrichments" value="enrichments" />
             </div>
             <div className="checkbox">
-              <label htmlFor="extractCover" className="col-sm-offset-1">Cover</label>
-              <input type="checkbox" name="extract" id="extractCover" value="cover" />
+                <label htmlFor="extractCover" className="col-sm-offset-1">Cover</label>
+                <input type="checkbox" name="extract" id="extractCover" value="cover" />
             </div>
             <div className="checkbox">
-              <label htmlFor="extractAnnexes" className="col-sm-offset-1">Annexes</label>
-              <input type="checkbox" name="extract" id="extractAnnexes" value="annexes" />
+                <label htmlFor="extractAnnexes" className="col-sm-offset-1">Annexes</label>
+                <input type="checkbox" name="extract" id="extractAnnexes" value="annexes" />
             </div>
             <div className="form-group">
-              <div className="col-sm-offset-1 col-sm-11">
-                <button type="submit" className="btn btn-primary">Télécharger</button>
-              </div>
+                <div className="col-sm-offset-1 col-sm-11">
+                    <button type="submit" className="btn btn-primary">Télécharger</button>
+                </div>
             </div>
             <input type="hidden" name="size" value="100" />
-          </form>
-        );
-    }
+        </form>
+    );
 }
-
+export default Form;

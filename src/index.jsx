74dd59be79721bf7e74a4511d0a9ee0ef58db4bc
François Parmentier <francois.parmentier@gmail.com>
2017-08-30 11:52:41 +0200

    :shirt: A bit of linting

--- src/index.jsx
+++ src/index.jsx
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Form from './Form.jsx';
 
-ReactDOM.render(<Form />,  document.getElementById('root'));
+ReactDOM.render(<Form />, document.getElementById('root'));

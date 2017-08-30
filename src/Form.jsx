1a0a6c42338dc386a39f517af8c8188bad13256c
Nicolas Thouvenin <nthouvenin@gmail.com>
2017-08-30 10:50:11 +0200

    build url in react

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,44 +1,126 @@
 import React from 'react';
+import { URL } from 'universal-url';
 
-function Form() {
-    return (
-        <form action="https://api.istex.fr/document/" method="get" className="form-horizontal">
-            <div className="form-group">
-                <label htmlFor="q" className="col-sm-1 control-label">Requête</label>
-                <div className="col-sm-11">
-                    <textarea
-                        name="q" id="q" rows="3" autoFocus="true" className="form-control"
-                        placeholder="brain surgery"
+
+export default class Form extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            q: '',
+            extractMetadata: false,
+            extractFulltext: false,
+            extractEnrichments: false,
+            extractCover: false,
+            extractAnnexes: false,
+        };
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value,
+        });
+    }
+
+    handleSubmit(event) {
+        const toAPI = new URL('https://api.istex.fr/document/');
+        let extract = '';
+
+        if (this.state.extractMetadata) {
+            extract = extract.concat('metadata;');
+        }
+        if (this.state.extractFulltext) {
+            extract = extract.concat('fulltext;');
+        }
+
+        toAPI.searchParams.set('q', this.state.q);
+        toAPI.searchParams.set('extract', extract);
+
+        window.location = toAPI.href;
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <form className="form-horizontal" onSubmit={this.handleSubmit}>
+                <div className="form-group">
+                    <label htmlFor="q" className="col-sm-1 control-label">Requête</label>
+                    <div className="col-sm-11">
+                        <textarea
+                            className="form-control"
+                            name="q"
+                            id="q"
+                            rows="3"
+                            autoFocus="true"
+                            value={this.state.q}
+                            onChange={this.handleInputChange}
+                        />
+                    </div>
+                </div>
+                <div className="checkbox">
+                    <label htmlFor="extractMetadata" className="col-sm-offset-1">Métadonnées</label>
+                    <input
+                        type="checkbox"
+                        id="extractMetadata"
+                        name="extractMetadata"
+                        checked={this.state.extractMetadata}
+                        onChange={this.handleInputChange}
                     />
                 </div>
-            </div>
-            <div className="checkbox">
-                <label htmlFor="extractMetadata" className="col-sm-offset-1">Métadonnées</label>
-                <input type="checkbox" name="extract" id="extractMetadata" value="metadata" />
-            </div>
-            <div className="checkbox">
-                <label htmlFor="extractFulltext" className="col-sm-offset-1">Fulltext</label>
-                <input type="checkbox" name="extract" id="extractFulltext" value="fulltext" />
-            </div>
-            <div className="checkbox">
-                <label htmlFor="extractEnrichments" className="col-sm-offset-1">Enrichments</label>
-                <input type="checkbox" name="extract" id="extractEnrichments" value="enrichments" />
-            </div>
-            <div className="checkbox">
-                <label htmlFor="extractCover" className="col-sm-offset-1">Cover</label>
-                <input type="checkbox" name="extract" id="extractCover" value="cover" />
-            </div>
-            <div className="checkbox">
-                <label htmlFor="extractAnnexes" className="col-sm-offset-1">Annexes</label>
-                <input type="checkbox" name="extract" id="extractAnnexes" value="annexes" />
-            </div>
-            <div className="form-group">
-                <div className="col-sm-offset-1 col-sm-11">
-                    <button type="submit" className="btn btn-primary">Télécharger</button>
+                <div className="checkbox">
+                    <label htmlFor="extractFulltext" className="col-sm-offset-1">Fulltext</label>
+                    <input
+                        type="checkbox"
+                        id="extractFulltext"
+                        name="extractFulltext"
+                        checked={this.state.extractFulltext}
+                        onChange={this.handleInputChange}
+                    />
                 </div>
-            </div>
-            <input type="hidden" name="size" value="100" />
-        </form>
-    );
+                <div className="checkbox">
+                    <label htmlFor="extractEnrichments" className="col-sm-offset-1">Enrichments</label>
+                    <input
+                        type="checkbox"
+                        id="extractEnrichments"
+                        name="extractEnrichments"
+                        checked={this.state.extractEnrichments}
+                        onChange={this.handleInputChange}
+                    />
+                </div>
+                <div className="checkbox">
+                    <label htmlFor="extractCover" className="col-sm-offset-1">Cover</label>
+                    <input
+                        type="checkbox"
+                        id="extractCover"
+                        name="extractCover"
+                        checked={this.state.extractCover}
+                        onChange={this.handleInputChange}
+                    />
+                </div>
+                <div className="checkbox">
+                    <label htmlFor="extractAnnexes" className="col-sm-offset-1">Annexes</label>
+                    <input
+                        type="checkbox"
+                        id="extractAnnexes"
+                        name="extractAnnexes"
+                        checked={this.state.extractAnnexes}
+                        onChange={this.handleInputChange}
+                    />
+                </div>
+                <div className="form-group">
+                    <div className="col-sm-offset-1 col-sm-11">
+                        <button type="submit" className="btn btn-primary">Télécharger</button>
+                    </div>
+                </div>
+                <input type="hidden" name="size" value="100" />
+            </form>
+        );
+    }
 }
-export default Form;

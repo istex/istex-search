acc8caed60cf6732593cd00d7eead0fa34fa7936
Stéphane Gully <stephane.gully@gmail.com>
2017-08-31 15:17:24 +0200

    no more dynamic logo

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 import Form from './Form/';
 import './App.css';
-import logo from './logo.svg';
 
 function App() {
     return (

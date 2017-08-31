3be14a96af690c4fd4f07b240efdc9e629645d49
Nicolas Thouvenin <nthouvenin@gmail.com>
2017-08-31 14:00:26 +0200

    add modal

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import InputRange from 'react-input-range';
+import { Modal, Button } from 'react-bootstrap';
 import 'react-input-range/lib/css/index.css';
 import './style.css';
 import Filetype from '../Filetype';
@@ -11,15 +13,19 @@ export default class Form extends React.Component {
         this.state = {
             q: '',
             size: 1,
-            extractMetadata: false,
+            extractMetadata: true,
             extractFulltext: false,
             extractEnrichments: false,
             extractCover: false,
             extractAnnexes: false,
+            downloading: false,
+            URL2Download: '',
         };
 
         this.handleQueryChange = this.handleQueryChange.bind(this);
         this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleCancel = this.handleCancel.bind(this);
+        this.handleSave = this.handleSave.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
@@ -59,7 +65,27 @@ export default class Form extends React.Component {
 
     handleSubmit(event) {
         const { href } = this.buildURLFromState();
-        window.location = href;
+        this.setState({
+            downloading: true,
+            URL2Download: href,
+        });
+        event.preventDefault();
+    }
+
+    handleCancel(event) {
+        this.setState({
+            downloading: false,
+            URL2Download: '',
+        });
+        event.preventDefault();
+    }
+
+    handleSave(event) {
+        this.setState({
+            downloading: false,
+            URL2Download: '',
+        });
+        window.location = this.state.URL2Download;
         event.preventDefault();
     }
 
@@ -83,55 +109,80 @@ export default class Form extends React.Component {
 
     render() {
         return (
-            <form className="form-horizontal" onSubmit={this.handleSubmit}>
-                <div className="form-group">
-                    <label htmlFor="q" className="col-sm-1 control-label">Requête</label>
-                    <div className="col-sm-11">
-                        <textarea
-                            className="form-control"
-                            name="q"
-                            id="q"
-                            rows="3"
-                            autoFocus="true"
-                            value={this.state.q}
-                            onChange={this.handleQueryChange}
-                        />
+            <div className={this.props.className}>
+                <form className="form-horizontal" onSubmit={this.handleSubmit}>
+                    <div className="form-group">
+                        <label htmlFor="q" className="col-sm-1 control-label">Requête</label>
+                        <div className="col-sm-11">
+                            <textarea
+                                className="form-control"
+                                name="q"
+                                id="q"
+                                rows="3"
+                                autoFocus="true"
+                                value={this.state.q}
+                                onChange={this.handleQueryChange}
+                            />
+                        </div>
                     </div>
-                </div>
-                <div className="form-group">
-                    <span className="col-sm-1 control-label">Documents à télécharger</span>
-                    <div className="col-sm-11">
-                        {this.state.total}
+                    <div className="form-group">
+                        <span className="col-sm-1 control-label">Documents à télécharger</span>
+                        <div className="col-sm-11">
+                            {this.state.total}
+                        </div>
                     </div>
-                </div>
-                <Filetype
-                    label="Métadonnées"
-                    filetype="metadata"
-                    formats="mods,tei"
-                    labels="MODS|Text Encoding Initiative"
-                />
-                <div className="form-group">
-                    <div className="col-sm-10">
-                        <div className="checkbox">
-                            <label htmlFor="size" className="col-sm-1">Size</label>
-                            <div className="col-sm-1">
-                                <InputRange
-                                    id="size"
-                                    maxValue={200}
-                                    minValue={0}
-                                    value={this.state.size}
-                                    onChange={size => this.setState({ size })}
-                                />
+                    <Filetype
+                        label="Métadonnées"
+                        filetype="metadata"
+                        formats="mods,tei"
+                        labels="MODS|Text Encoding Initiative"
+                    />
+                    <div className="form-group">
+                        <div className="col-sm-10">
+                            <div className="checkbox">
+                                <label htmlFor="size" className="col-sm-1">Size</label>
+                                <div className="col-sm-1">
+                                    <InputRange
+                                        id="size"
+                                        maxValue={200}
+                                        minValue={0}
+                                        value={this.state.size}
+                                        onChange={size => this.setState({ size })}
+                                    />
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div className="form-group">
-                    <div className="col-sm-offset-1 col-sm-11">
-                        <button type="submit" className="btn btn-primary">Télécharger</button>
+                    <div className="form-group">
+                        <div className="col-sm-offset-1 col-sm-11">
+                            <button type="submit" className="btn btn-primary">Télécharger</button>
+                        </div>
                     </div>
-                </div>
-            </form>
+                </form>
+                <Modal show={this.state.downloading} onHide={this.close}>
+                    <Modal.Header>
+                        <Modal.Title>Téléchargement</Modal.Title>
+                    </Modal.Header>
+
+                    <Modal.Body>
+                        La génération de votre corpus est en cours...
+                    </Modal.Body>
+
+                    <Modal.Footer>
+                        <Modal.Footer>
+                            <Button onClick={this.handleCancel}>Annuler</Button>
+                            <Button onClick={this.handleSave}>Enregistrer</Button>
+                        </Modal.Footer>
+                    </Modal.Footer>
+                </Modal>
+            </div>
         );
     }
 }
+
+Form.defaultProps = {
+    className: '',
+};
+Form.propTypes = {
+    className: PropTypes.string,
+};

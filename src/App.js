faa3a8145c735f1a18c71e3d0cc1ffd3c7a10e43
camilledesalabert <camille.de-salabert@inist.fr>
2017-12-21 13:43:36 +0100

    header text improved

--- src/App.js
+++ src/App.js
@@ -27,8 +27,8 @@ export default class App extends React.Component {
                             <h1>Téléchargez un corpus ISTEX</h1>
                             <p className="lead">
                                 Vous êtes membre de l’Enseignement Supérieur et de la Recherche et
-                                vous souhaitez extraire un corpus de documents Istex&nbsp;?
-                                3 étapes suffisent et vous pouvez récupérer une archive zip sur votre disque dur.
+                                vous souhaitez extraire un corpus de documents Istex&nbsp;? <br>
+                                3 étapes suffisent pour récupérer une archive zip sur votre disque dur.
                             </p>
                         </div>
                         <div className="col-lg-2">

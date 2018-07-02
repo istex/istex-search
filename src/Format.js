2202f6a11b9e53ba6b8e7c4a9736df3f66b709cd
camilledesalabert <camille.de-salabert@inist.fr>
2018-07-02 09:42:12 +0200

    adding tooltips on metadata

--- src/Format.js
+++ src/Format.js
@@ -76,6 +76,20 @@ export default class Format extends React.Component {
         let tooltipText = '';
         if (this.props.withPopover) {
             switch (this.props.label) {
+            case 'xml': tooltipText = (
+                <p>
+                    <a href="https://istex-doc.gitbook.io/documentation-istex/tdm/annexes/liste-des-formats#o-xml-extensible-markup-language-langage-de-balisage-extensible" target="_blank" rel="noopener noreferrer"> Extensible Markup Language (langage de balisage extensible) </a>
+                    + 'Fichier original fourni par l\'éditeur selon une DTD propre.
+                </p>
+                );
+                break;        
+            case 'mods': tooltipText = (
+                <p>
+                   <a href="https://doc.istex.fr/tdm/annexes/liste-des-formats.html#o---mods--metadata-object-description-schema-sch%C3%A9ma-de-description-objet-de-m%C3%A9tadonn%C3%A9es" target="_blank" rel="noopener noreferrer"> Metadata Object Description Schema (schéma de description objet de métadonnées) </a>
+                    + Fichier XML produit par ISTEX à partir d’une transformation des XML originaux, afin de les homogénéiser dans le format standard de description de métadonnées MODS (version 3.6).
+                </p>
+                );
+                break;
             case 'nb': tooltipText = (
                 <p>
                     <a href="https://inist-category.data.istex.fr" target="_blank" rel="noopener noreferrer">Catégories scientifiques Inist</a> des bases Pascal et Francis,
@@ -92,19 +106,19 @@ export default class Format extends React.Component {
                 </p>
                 );
                 break;
-            case 'teeft': tooltipText = (
-                <p>
-                    Termes d’indexation, extraits des documents en texte intégral grâce à l’outil&nbsp;
-                     <a href="https://enrichment-process.data.istex.fr/ark:/67375/R0H-R25KK4KZ-Q" target="_blank" rel="noopener noreferrer">teeft</a>.
-                </p>
-                );
-                break;
             case 'refBibs': tooltipText = (
                 <p>
                     Références bibliographiques des documents, structurées à l’aide de l’outil <a href="https://enrichment-process.data.istex.fr/ark:/67375/R0H-2WXX0NK2-9" target="_blank" rel="noopener noreferrer">Grobid</a>
                 </p>
                 );
                 break;
+            case 'teeft': tooltipText = (
+                <p>
+                    Termes d’indexation, extraits des documents en texte intégral grâce à l’outil&nbsp;
+                     <a href="https://enrichment-process.data.istex.fr/ark:/67375/R0H-R25KK4KZ-Q" target="_blank" rel="noopener noreferrer">teeft</a>.
+                </p>
+                );
+                break;        
             case 'unitex': tooltipText = (
                 <p>
                     <a href="https://named-entity.data.istex.fr" target="_blank" rel="noopener noreferrer">Entités nommées Istex</a>, extraites des documents à l&apos;aide du logiciel <a href="https://enrichment-process.data.istex.fr/ark:/67375/R0H-KGDTPS40-S" target="_blank" rel="noopener noreferrer">Unitex-CasSys</a>.

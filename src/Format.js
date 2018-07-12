60c0403e2fb09980172dbbffbeca23385f7a85e3
camilledesalabert <camille.de-salabert@inist.fr>
2018-07-12 11:48:14 +0200

    ocr tooltip text correction

--- src/Format.js
+++ src/Format.js
@@ -131,7 +131,7 @@ export default class Format extends React.Component {
                     </a> 
                     <br />
                     Fichier produit par ISTEX grâce à une chaîne de réocérisation qui fonctionne par reconnaissance
-                    optique de caractères pour améliorer la qualité des documents originaux. À utiliser s’il est disponible à la place
+                    optique de caractères pour améliorer la qualité des documents originaux. À utiliser s’il est présent à la place
                     du fichier TXT.
                 </p>
                 );

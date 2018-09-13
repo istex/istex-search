ebeae150a42b508fd6abe21461e766881c3b8014
fjoin <florence.join@gmail.com>
2018-09-13 15:29:47 +0200

    design change

--- src/storageHistory.js
+++ src/storageHistory.js
@@ -128,7 +128,7 @@ export default class storageHistory extends React.Component {
                                         window.location = `${this.localStorage[i].url}&download=true`;
                                     }}
                                 >
-                                    <img src="télécharger-noir.png"/>
+                                    <img src="telecharger-noir.png"/>
                                 </OverlayTrigger>
                                 <OverlayTrigger
                                     placement="top"

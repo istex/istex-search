0ff76d4e174625d3be7d8e865275a4736d4df30a
niederlec <claude.niederlender@gmail.com>
2020-08-28 15:56:08 +0200

    increase test version number

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 /* eslint-disable comma-dangle */
 const config = {
-    apiUrl: 'https://api.istex.fr',
-    dlIstexUrl: 'https://dl.istex.fr/',
+    apiUrl: 'https://api-integ.istex.fr',
+    dlIstexUrl: 'https://istexdl-421b7-1.demo-dev.istex.fr/',
     limitNbDoc: 100000,
     defaultSize: 0,
-    characterLimit: 7000
+    characterLimit: 2000
 };
 export default config;

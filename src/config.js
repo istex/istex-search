499624b6273ab46fd50673268e026e2210d23e69
niederlec <claude.niederlender@gmail.com>
2020-10-06 10:30:06 +0200

    restore production-ready config

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 /* eslint-disable comma-dangle */
 const config = {
-    apiUrl: 'https://api-integ.istex.fr',
-    dlIstexUrl: 'https://istexdl-421b7-1.demo-dev.istex.fr/',
+    apiUrl: 'https://api.istex.fr',
+    dlIstexUrl: 'https://dl.istex.fr/',
     limitNbDoc: 100000,
     defaultSize: 0,
     characterLimit: 2000

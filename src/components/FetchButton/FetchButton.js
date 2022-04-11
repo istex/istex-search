38b91e9fdb4e4af680b21782c0d7c7128e55c3e9
Clément Dreptin <cdreptin@gmail.com>
2022-04-11 16:57:00 +0200

    fix(fetch-button): fixed the fetch button being enabled even when the history was empty

--- src/components/FetchButton/FetchButton.js
+++ src/components/FetchButton/FetchButton.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import eventEmitter from '../../lib/eventEmitter';
 import localStorage from '../../lib/localStorage';
 
 export default function FetchButton () {
+  const [isButtonEnabled, enableButton] = useState(!!localStorage.get(0));
+
   const updateFormFromLastRequestInLocalStorage = () => {
     const mostRecentRequest = localStorage.get(0);
+
+    if (!mostRecentRequest) {
+      return;
+    }
 
     if (mostRecentRequest.qId) {
       eventEmitter.emit('qIdChanged', mostRecentRequest.qId);
@@ -20,9 +26,22 @@ export default function FetchButton () {
     eventEmitter.emit('archiveTypeChanged', mostRecentRequest.archiveType);
   };
 
+  const localStorageUpdatedHandler = () => {
+    enableButton(!!localStorage.get(0));
+  };
+
+  useEffect(() => {
+    eventEmitter.addListener('localStorageUpdated', localStorageUpdatedHandler);
+  }, []);
+
   return (
     <div>
-      <button onClick={updateFormFromLastRequestInLocalStorage}>Fetch</button>
+      <button
+        onClick={updateFormFromLastRequestInLocalStorage}
+        disabled={!isButtonEnabled}
+      >
+        Fetch
+      </button>
     </div>
   );
 }

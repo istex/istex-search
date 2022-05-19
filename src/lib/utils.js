950f79741cacb9a2dc0809f4f1d6aed1c340ea3e
Clément Dreptin <cdreptin@gmail.com>
2022-05-19 15:36:01 +0200

    feat(utils): added a asyncDebounce function create debounced function that return promises

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -27,3 +27,28 @@ export function debounce (callback, delay = 1000) {
     }, delay);
   };
 }
+
+/**
+ * Debounce `callback` after delay. The returned function returns a `Promise`.
+ * @param {function} callback The asynchronous callback (must return a `Promise`).
+ * @param {number} delay The delay to wait before calling `callback`.
+ * @returns A function that will call its callback only if `delay` has passed since the last time it was called.
+ *          This function returns a `Promise` that will resolve when the `Promise` returned by its callback resolves.
+ */
+export function asyncDebounce (callback, delay = 1000) {
+  const debounced = debounce(async (resolve, reject, args) => {
+    try {
+      // eslint-disable-next-line node/no-callback-literal
+      const result = await callback(...args);
+      resolve(result);
+    } catch (error) {
+      reject(error);
+    }
+  }, delay);
+
+  return (...args) => {
+    return new Promise((resolve, reject) => {
+      debounced(resolve, reject, args);
+    });
+  };
+}

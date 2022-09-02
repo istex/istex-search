9df7a0ea97077925572320d75389c1b377c8a17c
Steph-W <nkuinjeustephane@gmail.com>
2022-09-02 08:09:08 +0200

    [FEATURE] Add system on notification (#122)
    * [FEATURE] Add system on notification
    
    * [FIX] Close modal when uses clicks on copy button inside share modal

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faEraser, faRepeat, faLink, faClockRotateLeft, faCircleInfo } from '@fortawesome/free-solid-svg-icons';
+import { faEraser, faRepeat, faLink, faClockRotateLeft, faCircleInfo, faExclamationCircle, faTriangleExclamation } from '@fortawesome/free-solid-svg-icons';
 
 import './index.scss';
 import './assets/styles/styles.scss';
 import 'flowbite';
+import 'react-toastify/dist/ReactToastify.min.css';
 
 import App from './components/App/App';
 import store from './store/store';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
-library.add(faEraser, faRepeat, faLink, faClockRotateLeft, faCircleInfo);
+library.add(faEraser, faRepeat, faLink, faClockRotateLeft, faCircleInfo, faExclamationCircle, faTriangleExclamation);
 
 ReactDOM.render(
   <BrowserRouter>

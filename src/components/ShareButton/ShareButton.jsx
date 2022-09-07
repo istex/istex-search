a2a4b0f7b6183d97c96ad51e6d61a7f520770e0a
Stephane NKUINJEU <nkuinjeustephane@gmail.com>
2022-09-07 14:44:06 +0200

    [#29] Fix url on share modal button

--- src/components/ShareButton/ShareButton.jsx
+++ src/components/ShareButton/ShareButton.jsx
@@ -39,7 +39,7 @@ export default function ShareButton () {
 
   const handleSaveToClipboard = () => {
     setOpenModal(false);
-    navigator.clipboard.writeText(window.location.href)
+    navigator.clipboard.writeText(urlToClipboard)
       .then(() => eventEmitter.emit(events.displayNotification, { text: 'le lien a été copié dans le presse-papier' }))
       .catch(() => eventEmitter.emit(events.displayNotification, {
         text: 'Une erreur est survenue, veuillez réessayer ultérieurement !',

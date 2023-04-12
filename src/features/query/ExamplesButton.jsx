f8a2e985967f7323bf1b26aed92dc296d7dfe145
Clément Dreptin <cdreptin@gmail.com>
2023-04-12 13:18:50 +0200

    refactor(examples): stopped using the flowbite Modal component

--- src/features/query/ExamplesButton.jsx
+++ src/features/query/ExamplesButton.jsx
@@ -1,19 +1,20 @@
 import React, { useState } from 'react';
-import { Tooltip, Modal } from 'flowbite-react';
+import { Tooltip } from 'flowbite-react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import Example from './Example';
+import Modal from '@/components/Modal';
 
 import { examples } from '@/config';
 
 export default function ExamplesButton () {
-  const [modalOpened, showModal] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
 
   return (
     <>
       <button
         className='!px-6 cta-blue-wired font-montserrat font-medium flex gap-2'
-        onClick={() => showModal(true)}
+        onClick={() => setModalOpen(true)}
       >
         <span>Exemples</span>
         <div className='m-auto'>
@@ -23,35 +24,27 @@ export default function ExamplesButton () {
         </div>
       </button>
 
-      <Modal show={modalOpened} onClose={() => showModal(false)}>
-        <div className='istex-modal__header'>
-          <Modal.Header>
-            <span className='istex-modal__text'>Exemples de requêtes</span>
-          </Modal.Header>
-        </div>
-        <Modal.Body>
-          <p className='pb-3 text-sm text-istcolor-grey-link'>
-            Voici quelques exemples dont vous pouvez vous inspirer pour votre recherche. Cliquez sur l'une des loupes et la zone de requête sera remplie automatiquement par le contenu de l'exemple choisi. Cet échantillon illustre différentes façons d'interroger l'API Istex en utilisant :
-          </p>
-          <div className='flex flex-col justify-between overflow-auto h-72'>
-            {examples.map(example => (
-              <div key={example.label}>
-                <Example info={example} closeModal={() => showModal(false)} />
+      {modalOpen && (
+        <Modal onClose={() => setModalOpen(false)}>
+          <Modal.Header>Exemples de requêtes</Modal.Header>
+          <Modal.Body>
+            <div className='p-1 flex flex-col gap-2'>
+              <p className='max-w-2xl'>
+                Voici quelques exemples dont vous pouvez vous inspirer pour votre recherche. Cliquez sur l'une des loupes et la zone de requête sera remplie automatiquement par le contenu de l'exemple choisi. Cet échantillon illustre différentes façons d'interroger l'API Istex en utilisant :
+              </p>
+              <div className=''>
+                {examples.map(example => (
+                  <div key={example.label}>
+                    <Example info={example} closeModal={() => setModalOpen(false)} />
+                    <Example info={example} closeModal={() => setModalOpen(false)} />
+                  </div>
+                ))}
               </div>
-            ))}
-          </div>
-        </Modal.Body>
-        <Modal.Footer>
-          <div className='flex w-full justify-end'>
-            <button
-              onClick={() => showModal(false)}
-              className='p-2 text-white bg-istcolor-blue border border-istcolor-blue cta1 focus:ring-4 focus:outline-none'
-            >
-              Annuler
-            </button>
-          </div>
-        </Modal.Footer>
-      </Modal>
+            </div>
+          </Modal.Body>
+          <Modal.Footer />
+        </Modal>
+      )}
     </>
   );
 }

881f05ac7d5b8b639fa3a31dba2c7b2e16f33a16
Clément Dreptin <44998961+ClementDreptin@users.noreply.github.com>
2023-04-19 15:12:09 +0200

    refactor: bottom menu (#227)
    * refactor(reset): remove the wrapping button element and used a button instead of a div inside of ResetButton
    
    * refactor(reset): moved the tooltip inside of the ResetButton component
    
    * refactor(share): removed unnecessary div wrapper and stopped centering the ShareButton tooltip text
    
    * refactor(history): removed unnecessary div wrapper and stopped centering the HistoryButton tooltip text
    
    * refactor(fetch): removed the wrapping button element around FetchButton
    
    * refactor(fetch): moved the tooltip inside of FetchButton

--- src/components/BottomMenu.jsx
+++ src/components/BottomMenu.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Tooltip } from 'flowbite-react';
 
 import ResetButton from '@/features/resetForm/ResetButton';
 import FetchButton from '@/features/fetch/FetchButton';
@@ -10,38 +9,10 @@ export default function BottomMenu () {
   return (
     <div className='!fixed border-0 w-full bottom-0 left-0 bg-istcolor-green-dark py-0.5 h-20 z-10'>
       <div className='flex justify-center gap-2 md:gap-x-14 items-center'>
-        <Tooltip
-          content={(
-            <div className='max-w-[10rem] text-center'>
-              Effacez tout pour redémarrer avec un formulaire vide
-            </div>
-          )}
-        >
-          <button>
-            <ResetButton />
-          </button>
-        </Tooltip>
-        <Tooltip
-          content={(
-            <div className='max-w-[8rem] text-center'>
-              Récupérez l'état en cours de votre formulaire
-            </div>
-          )}
-        >
-          <button>
-            <FetchButton />
-          </button>
-        </Tooltip>
-        {/* We don't use button here because of bug. the button adds a focus
-          to the click which prevents the tooltip from disappearing after opening the modal */}
-        <div>
-          <ShareButton />
-        </div>
-        {/* We don't use button here because of bug. the button adds a focus
-          to the click which prevents the tooltip from disappearing after opening the modal */}
-        <div>
-          <HistoryButton />
-        </div>
+        <ResetButton />
+        <FetchButton />
+        <ShareButton />
+        <HistoryButton />
       </div>
     </div>
   );
--- src/features/resetForm/ResetButton.jsx
+++ src/features/resetForm/ResetButton.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Tooltip } from 'flowbite-react';
 
 import useResetForm from './useResetForm';
 
@@ -7,14 +8,22 @@ export default function ResetButton () {
   const resetForm = useResetForm();
 
   return (
-    <div
-      className='flex flex-col justify-between items-center cursor-pointer hover:bg-istcolor-white hover:rounded-md p-2.5 h-[4.75rem] text-istcolor-black'
-      onClick={resetForm}
+    <Tooltip
+      content={(
+        <div className='max-w-[8rem]'>
+          Effacez tout pour redémarrer avec un formulaire vide
+        </div>
+      )}
     >
-      <div>
-        <FontAwesomeIcon icon='eraser' className='text-3xl md:text-4xl' />
-      </div>
-      <span className='text-center align-top'>Réinitialiser</span>
-    </div>
+      <button
+        className='flex flex-col justify-between items-center cursor-pointer hover:bg-istcolor-white hover:rounded-md p-2.5 h-[4.75rem] text-istcolor-black'
+        onClick={resetForm}
+      >
+        <div>
+          <FontAwesomeIcon icon='eraser' className='text-3xl md:text-4xl' />
+        </div>
+        <span className='text-center align-top'>Réinitialiser</span>
+      </button>
+    </Tooltip>
   );
 }

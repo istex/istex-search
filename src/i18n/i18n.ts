8e9b449d44d795748bed7c387a2cb0c62f950d4e
Clément Dreptin <cdreptin@gmail.com>
2023-06-01 16:05:08 +0200

    fix(i18n): stopped merging translations to fix inconsistent label displaying

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -1,16 +1,7 @@
 import { getRequestConfig } from 'next-intl/server';
-import deepmerge from 'deepmerge';
-import { DEFAULT_LOCALE } from './constants';
 
 export default getRequestConfig(async ({ locale }) => {
-  let translations = (await import(`./translations/${locale}.json`)).default;
-
-  // If the current locale isn't the default one, merge the default translations
-  // into it to make sure something a label is always displayed
-  if (locale !== DEFAULT_LOCALE) {
-    const defaultTranslations = (await import(`./translations/${DEFAULT_LOCALE}.json`)).default;
-    translations = deepmerge(defaultTranslations, translations);
-  }
+  const translations = (await import(`./translations/${locale}.json`)).default;
 
   return {
     messages: translations,

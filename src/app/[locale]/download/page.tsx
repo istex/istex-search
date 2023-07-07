5d7eb9d348dbd9ff04c0e37121c57d11cbd8ab95
Clément Dreptin <cdreptin@gmail.com>
2023-07-07 15:37:05 +0200

    feat: added metadata title to the download page

--- src/app/[locale]/download/page.tsx
+++ src/app/[locale]/download/page.tsx
@@ -1,6 +1,15 @@
+import { getTranslations } from "next-intl/server";
 import { Container } from "@/mui/material";
 import DownloadForm from "./components/DownloadForm";
-import type { Page } from "@/types/next";
+import type { GenerateMetadata, Page } from "@/types/next";
+
+export const generateMetadata: GenerateMetadata = async () => {
+  const t = await getTranslations("download.metadata");
+
+  return {
+    title: `Istex-DL - ${t("title")}`,
+  };
+};
 
 const DownloadPage: Page = () => {
   return (

37938414f34dfbed2f85f1bd863efd254e4ee441
Clément Dreptin <cdreptin@gmail.com>
2023-07-11 13:42:52 +0200

    feat: styled the usage selector

--- src/app/[locale]/download/components/DownloadForm.tsx
+++ src/app/[locale]/download/components/DownloadForm.tsx
@@ -37,7 +37,7 @@ const DownloadForm: ServerComponent = () => {
           </Panel>
         </Grid>
         <Grid item xs={3} container spacing={2} direction="column">
-          <Grid item xs>
+          <Grid item>
             <Panel>
               <strong>Gateway info</strong>
               <br />
@@ -47,7 +47,7 @@ const DownloadForm: ServerComponent = () => {
               </Typography>
             </Panel>
           </Grid>
-          <Grid item xs>
+          <Grid item>
             <Panel>
               <strong>Query info</strong>
               <br />
@@ -60,26 +60,6 @@ const DownloadForm: ServerComponent = () => {
               </Typography>
             </Panel>
           </Grid>
-        </Grid>
-        <Grid item xs={3}>
-          <Panel>
-            <strong>Document list</strong>
-            <br />
-            <Typography variant="body2">
-              Lorem ipsum dolor sit amet, officia excepteur ex fugiat
-              reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit
-              ex esse exercitation amet. Nisi anim cupidatat excepteur officia.
-              Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet
-              voluptate voluptate dolor minim nulla est proident. Nostrud
-              officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex
-              occaecat reprehenderit commodo officia dolor Lorem duis laboris
-              cupidatat officia voluptate. Culpa proident adipisicing id nulla
-              nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua
-              reprehenderit commodo ex non excepteur duis sunt velit enim.
-              Voluptate laboris sint cupidatat ullamco ut ea consectetur et est
-              culpa et culpa duis.
-            </Typography>
-          </Panel>
         </Grid>
       </Grid>
     </>
--- src/app/[locale]/download/components/UsageSelector.tsx
+++ src/app/[locale]/download/components/UsageSelector.tsx
@@ -2,9 +2,52 @@
 
 import { useState } from "react";
 import { useTranslations } from "next-intl";
+import { montserrat } from "@/mui/fonts";
 import { Tab, Tabs } from "@/mui/material";
+import { styled } from "@mui/material/styles";
 import { usages, type Usage } from "@/config";
 import type { ClientComponent } from "@/types/next";
+
+const StyledTabs = styled(Tabs)(({ theme }) => ({
+  "&.MuiTabs-root": {
+    borderRadius: 999,
+    backgroundColor: theme.palette.colors.white,
+    padding: theme.spacing(1),
+    minHeight: "fit-content",
+  },
+  "& .MuiTabs-flexContainer": {
+    height: "100%",
+  },
+  "& .MuiTabs-indicator": {
+    top: 3,
+    bottom: 3,
+    right: 3,
+    height: "auto",
+    background: "none",
+    "&:after": {
+      content: '""',
+      display: "block",
+      position: "absolute",
+      top: 0,
+      left: 4,
+      right: 4,
+      bottom: 0,
+      borderRadius: 999,
+      backgroundColor: "white",
+      boxShadow: "0 4px 5px 0 rgba(0, 0, 0, 0.1)",
+    },
+  },
+}));
+
+const StyledTab = styled(Tab)(({ theme }) => ({
+  "&.MuiTab-root": {
+    fontFamily: montserrat.style.fontFamily,
+    padding: `${theme.spacing(1)} ${theme.spacing(2)}`,
+    minHeight: "fit-content",
+    borderRadius: 999,
+    zIndex: 1,
+  },
+}));
 
 const UsageSelector: ClientComponent = () => {
   const t = useTranslations("config.usages");
@@ -20,11 +63,16 @@ const UsageSelector: ClientComponent = () => {
   };
 
   return (
-    <Tabs value={currentUsage} onChange={handleChange} centered>
+    <StyledTabs value={currentUsage} onChange={handleChange} centered>
       {usages.map(({ name, label }) => (
-        <Tab key={name} value={name} label={t(label)} />
+        <StyledTab
+          key={name}
+          value={name}
+          label={t(label)}
+          disableTouchRipple
+        />
       ))}
-    </Tabs>
+    </StyledTabs>
   );
 };
 

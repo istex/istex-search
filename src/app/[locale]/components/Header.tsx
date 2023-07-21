ef8d534a95be330c9a594a37a4deee57d3a35735
Clément Dreptin <cdreptin@gmail.com>
2023-07-21 10:28:32 +0200

    feat(header): changed the baseline and removed the icons

--- src/app/[locale]/components/Header.tsx
+++ src/app/[locale]/components/Header.tsx
@@ -1,25 +1,10 @@
 import { useTranslations } from "next-intl";
 import Image from "next/image";
-import DownloadIcon from "@mui/icons-material/Download";
-import { Avatar, Box, Container, Typography } from "@mui/material";
+import { Box, Container, Typography } from "@mui/material";
 import headerBackground from "@/../public/header-background.jpg";
 import istexDlLogo from "@/../public/istex-dl.svg";
+import Link from "@/i18n/next-intl-link";
 import type { ServerComponent } from "@/types/next";
-
-const icons = [
-  {
-    component: DownloadIcon,
-    title: "icons.0",
-  },
-  {
-    component: DownloadIcon,
-    title: "icons.1",
-  },
-  {
-    component: DownloadIcon,
-    title: "icons.2",
-  },
-];
 
 const Header: ServerComponent = () => {
   const t = useTranslations("home.Header");
@@ -44,36 +29,23 @@ const Header: ServerComponent = () => {
           }}
         >
           <Box>
-            <Image src={istexDlLogo} alt="Istex-DL" />
+            <Link href="/">
+              <Image src={istexDlLogo} alt="Istex-DL" />
+            </Link>
             <Typography
               color="white"
+              component="h2"
               variant="subtitle1"
               sx={{
                 fontSize: "0.875rem",
-                fontWeight: "bold",
                 lineHeight: 1.25,
               }}
             >
-              {t("subtitle")}
+              {t.rich("baseline", {
+                strong: (chunks) => <strong>{chunks}</strong>,
+                br: () => <br />,
+              })}
             </Typography>
-          </Box>
-          <Box sx={{ display: "flex", gap: 2, mt: { xs: 2, sm: 0 } }}>
-            {icons.map((icon) => {
-              const title = t(icon.title);
-              return (
-                <Avatar
-                  key={title}
-                  title={title}
-                  sx={{
-                    m: "auto",
-                    bgcolor: "colors.white",
-                    color: "colors.lightBlack",
-                  }}
-                >
-                  <icon.component />
-                </Avatar>
-              );
-            })}
           </Box>
         </Box>
       </Container>

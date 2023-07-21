7b90917a09e76b5404ba546a04c7a665c5a23121
Clément Dreptin <cdreptin@gmail.com>
2023-07-21 10:17:12 +0200

    fix(mui): reverted back to the old EmotionCacheProvider to remove obscure key prop warning

--- src/mui/setup.tsx
+++ src/mui/setup.tsx
@@ -3,110 +3,38 @@
 import { useState } from "react";
 import { useServerInsertedHTML } from "next/navigation";
 import createCache from "@emotion/cache";
-import type {
-  EmotionCache,
-  Options as OptionsOfCreateCache,
-} from "@emotion/cache";
-import { CacheProvider as DefaultCacheProvider } from "@emotion/react";
+import { CacheProvider } from "@emotion/react";
 import CssBaseline from "@mui/material/CssBaseline";
 import ThemeProvider from "@mui/material/styles/ThemeProvider";
 import theme from "./theme";
 import type { ClientComponent } from "@/types/next";
 
-// The EmotionCacheProvider taken from here:
-// https://github.com/mui/material-ui/blob/master/examples/material-next-app-router-ts/src/components/ThemeRegistry/EmotionCache.tsx
-
-interface EmotionCacheProviderProps {
-  /** This is the options passed to createCache() from 'import createCache from "@emotion/cache"' */
-  options: Omit<OptionsOfCreateCache, "insertionPoint">;
-  /** By default <CacheProvider /> from 'import { CacheProvider } from "@emotion/react"' */
-  CacheProvider?: (props: {
-    value: EmotionCache;
-    children: React.ReactNode;
-  }) => React.JSX.Element | null;
-}
-
-const EmotionCacheProvider: ClientComponent<EmotionCacheProviderProps, true> = (
-  props
-) => {
-  const { options, CacheProvider = DefaultCacheProvider, children } = props;
-
-  const [registry] = useState(() => {
-    const cache = createCache(options);
+const EmotionCacheProvider: ClientComponent<Record<string, unknown>, true> = ({
+  children,
+}) => {
+  const [cache] = useState(() => {
+    const cache = createCache({ key: "css" });
     cache.compat = true;
-    const prevInsert = cache.insert;
-    let inserted: Array<{ name: string; isGlobal: boolean }> = [];
-    cache.insert = (...args) => {
-      const [selector, serialized] = args;
-      if (cache.inserted[serialized.name] === undefined) {
-        inserted.push({
-          name: serialized.name,
-          isGlobal: selector === "",
-        });
-      }
-      return prevInsert(...args);
-    };
-    const flush = () => {
-      const prevInserted = inserted;
-      inserted = [];
-      return prevInserted;
-    };
-    return { cache, flush };
+    return cache;
   });
 
-  useServerInsertedHTML(() => {
-    const inserted = registry.flush();
-    if (inserted.length === 0) {
-      return null;
-    }
-    let styles = "";
-    let dataEmotionAttribute = registry.cache.key;
+  useServerInsertedHTML(() => (
+    <style
+      key={cache.key}
+      data-emotion={`${cache.key} ${Object.keys(cache.inserted).join(" ")}`}
+      dangerouslySetInnerHTML={{
+        __html: Object.values(cache.inserted).join(" "),
+      }}
+    />
+  ));
 
-    const globals: Array<{
-      name: string;
-      style: string;
-    }> = [];
-
-    inserted.forEach(({ name, isGlobal }) => {
-      const style = registry.cache.inserted[name];
-
-      if (typeof style !== "boolean") {
-        if (isGlobal) {
-          globals.push({ name, style });
-        } else {
-          styles += style;
-          dataEmotionAttribute += ` ${name}`;
-        }
-      }
-    });
-
-    return (
-      <>
-        {globals.map(({ name, style }) => (
-          <style
-            key={name}
-            data-emotion={`${registry.cache.key}-global ${name}`}
-            dangerouslySetInnerHTML={{ __html: style }}
-          />
-        ))}
-        {styles !== "" && (
-          <style
-            key={registry.cache.key}
-            data-emotion={dataEmotionAttribute}
-            dangerouslySetInnerHTML={{ __html: styles }}
-          />
-        )}
-      </>
-    );
-  });
-
-  return <CacheProvider value={registry.cache}>{children}</CacheProvider>;
+  return <CacheProvider value={cache}>{children}</CacheProvider>;
 };
 
 const MuiSetup: ClientComponent<Record<string, unknown>, true> = ({
   children,
 }) => (
-  <EmotionCacheProvider options={{ key: "mui" }}>
+  <EmotionCacheProvider>
     <ThemeProvider theme={theme}>
       <CssBaseline />
       {children}

2dbad483936fc639f760c1e779b03ea9d5d9c1eb
Clément Dreptin <cdreptin@gmail.com>
2023-09-01 13:57:55 +0200

    refactor: renamed actualSize to resultsCount and created a getResultsCount function

--- src/app/[locale]/(search)/results/@modal/(..)download/page.tsx
+++ src/app/[locale]/(search)/results/@modal/(..)download/page.tsx
@@ -1,26 +1,17 @@
 import DownloadModal from "./DownloadModal";
 import DownloadForm from "@/app/[locale]/download/components/DownloadForm";
-import { getResults } from "@/lib/istexApi";
+import { getResultsCount } from "@/lib/istexApi";
 import useSearchParams from "@/lib/useSearchParams";
 import type { Page } from "@/types/next";
-
-async function getActualSize(queryString: string) {
-  // We send the same request as the results even thought we
-  // just need the total to hit the Next.js cache went we were
-  // on the results page before
-  const response = await getResults(queryString);
-
-  return response.total;
-}
 
 const DownloadPage: Page = async ({ searchParams: nextSearchParams }) => {
   const searchParams = useSearchParams(nextSearchParams);
   const queryString = searchParams.getQueryString();
-  const actualSize = await getActualSize(queryString);
+  const resultsCount = await getResultsCount(queryString);
 
   return (
     <DownloadModal>
-      <DownloadForm actualSize={actualSize} />
+      <DownloadForm resultsCount={resultsCount} />
     </DownloadModal>
   );
 };
--- src/app/[locale]/(search)/results/page.tsx
+++ src/app/[locale]/(search)/results/page.tsx
@@ -3,10 +3,8 @@ import DownloadButton from "./components/DownloadButton";
 import ResultCard from "./components/ResultCard";
 import ResultsGrid from "./components/ResultsGrid";
 import ErrorCard from "@/components/ErrorCard";
-import { istexApiConfig } from "@/config";
 import { getResults, type IstexApiResponse } from "@/lib/istexApi";
 import useSearchParams from "@/lib/useSearchParams";
-import { clamp } from "@/lib/utils";
 import type { GenerateMetadata, Page } from "@/types/next";
 
 async function getTranslatedResults(
@@ -49,11 +47,10 @@ const ResultsPage: Page = async ({
 
   try {
     const results = await getTranslatedResults(queryString, locale);
-    const maxSize = clamp(results.total, 0, istexApiConfig.maxSize);
 
     // If the size is 0, take maxSize even if it's technically greater than 0 so
     // that users get maxSize by default and not 0
-    const sizeToUse = size !== 0 ? clamp(size, 0, maxSize) : maxSize;
+    const sizeToUse = size !== 0 ? size : results.total;
 
     return (
       <>

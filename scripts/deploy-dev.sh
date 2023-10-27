b18e08ccb6aeb9c16623a8bccf396e7daeaba0bc
Clément Dreptin <clement.dreptin@inist.fr>
2023-10-27 12:35:39 +0000

    ci: auto deploy in dev environment

---
+++ scripts/deploy-dev.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+docker compose down
+if [ ! $? -eq 0 ]; then
+  exit 1
+fi
+
+DANGLING_IMAGES=$(docker images -q -f dangling=true)
+if [ "$DANGLING_IMAGES" ]; then
+  docker rmi $DANGLING_IMAGES
+  if [ ! $? -eq 0 ]; then
+    exit 1
+  fi
+fi
+
+docker compose up -d --build

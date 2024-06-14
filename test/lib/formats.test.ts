327569744ac2be92fce3e3b32d6e7961cebaff0e
Clément Dreptin <clement.dreptin@inist.fr>
2024-06-14 12:29:00 +0200

    refactor(formats): used a more descriptive structure for the tests

--- test/lib/formats.test.ts
+++ test/lib/formats.test.ts
@@ -6,156 +6,198 @@ import {
 import * as Module from "@/lib/formats";
 
 describe("Functions manipulating the available download formats", () => {
-  it("selectFormat", () => {
-    expect(
-      Module.selectFormat(formats.fulltext.pdf, formats.fulltext.txt),
-    ).toBe(formats.fulltext.pdf | formats.fulltext.txt);
+  describe("selectFormat", () => {
+    it("selects formats", () => {
+      expect(
+        Module.selectFormat(formats.fulltext.pdf, formats.fulltext.txt),
+      ).toBe(formats.fulltext.pdf | formats.fulltext.txt);
+    });
   });
 
-  it("deselectFormat", () => {
-    expect(
-      Module.deselectFormat(
-        formats.fulltext.pdf | formats.fulltext.txt,
-        formats.fulltext.txt,
-      ),
-    ).toBe(formats.fulltext.pdf);
+  describe("deselectFormat", () => {
+    it("deselects formats", () => {
+      expect(
+        Module.deselectFormat(
+          formats.fulltext.pdf | formats.fulltext.txt,
+          formats.fulltext.txt,
+        ),
+      ).toBe(formats.fulltext.pdf);
+    });
   });
 
-  it("toggleFormat", () => {
+  describe("toggleFormat", () => {
     const formatsWithTxt = formats.fulltext.pdf | formats.fulltext.txt;
     const formatsWithoutTxt = formats.fulltext.pdf;
 
-    expect(Module.toggleFormat(formatsWithoutTxt, formats.fulltext.txt)).toBe(
-      formatsWithTxt,
-    );
-
-    expect(Module.toggleFormat(formatsWithTxt, formats.fulltext.txt)).toBe(
-      formatsWithoutTxt,
-    );
-  });
-
-  it("isFormatSelected", () => {
-    const formatsWithTxt = formats.fulltext.pdf | formats.fulltext.txt;
-    const formatsWithoutTxt = formats.fulltext.pdf;
-
-    expect(Module.isFormatSelected(formatsWithTxt, formats.fulltext.txt)).toBe(
-      true,
-    );
-
-    expect(
-      Module.isFormatSelected(formatsWithoutTxt, formats.fulltext.txt),
-    ).toBe(false);
-  });
-
-  it("isWholeCategorySelected", () => {
-    const metadataFormats =
-      formats.metadata.json | formats.metadata.xml | formats.metadata.mods;
-
-    expect(Module.isWholeCategorySelected(metadataFormats, "metadata")).toBe(
-      true,
-    );
-
-    expect(
-      Module.isWholeCategorySelected(formats.metadata.json, "metadata"),
-    ).toBe(false);
-
-    expect(
-      Module.isWholeCategorySelected(
-        metadataFormats | formats.fulltext.pdf,
-        "metadata",
-      ),
-    ).toBe(true);
-  });
-
-  it("getWholeCategoryFormat", () => {
-    const fulltextFormats =
-      formats.fulltext.pdf |
-      formats.fulltext.tei |
-      formats.fulltext.txt |
-      formats.fulltext.cleaned |
-      formats.fulltext.zip |
-      formats.fulltext.tiff;
-
-    const metadataFormats =
-      formats.metadata.json | formats.metadata.xml | formats.metadata.mods;
-
-    const enrichmentsFormats =
-      formats.enrichments.multicat |
-      formats.enrichments.nb |
-      formats.enrichments.grobidFulltext |
-      formats.enrichments.refBibs |
-      formats.enrichments.teeft |
-      formats.enrichments.unitex;
-
-    expect(Module.getWholeCategoryFormat("fulltext")).toBe(fulltextFormats);
-    expect(Module.getWholeCategoryFormat("metadata")).toBe(metadataFormats);
-    expect(Module.getWholeCategoryFormat("enrichments")).toBe(
-      enrichmentsFormats,
-    );
-  });
-
-  it("buildExtractParamsFromFormats", () => {
-    const selectedFormats =
-      formats.fulltext.pdf |
-      formats.fulltext.txt |
-      formats.enrichments.grobidFulltext |
-      formats.metadata.json |
-      formats.others.annexes |
-      formats.others.covers;
-
-    const expectedExtractParams = [
-      /fulltext\[pdf,txt\]/,
-      /metadata\[json\]/,
-      /enrichments\[grobidFulltext\]/,
-      /covers/,
-      /annexes/,
-    ];
-
-    const extractParams = Module.buildExtractParamsFromFormats(selectedFormats);
-    expectedExtractParams.forEach((extractParam) => {
-      expect(extractParams).toMatch(extractParam);
+    it("selects a format when it's not already selected", () => {
+      expect(Module.toggleFormat(formatsWithoutTxt, formats.fulltext.txt)).toBe(
+        formatsWithTxt,
+      );
     });
 
-    expect(Module.buildExtractParamsFromFormats(NO_FORMAT_SELECTED)).toBe("");
+    it("deselects a format when it's already selected", () => {
+      expect(Module.toggleFormat(formatsWithTxt, formats.fulltext.txt)).toBe(
+        formatsWithoutTxt,
+      );
+    });
   });
 
-  it("parseExtractParams", () => {
-    const correct =
-      "fulltext[pdf,txt];metadata[json];enrichments[grobidFulltext];covers;annexes";
-    const wrongFormat = "fulltext[incorrect];metadata[json]";
-    const wrongCategory = "fulltext[pdf];incorrect[json]";
-    const missingComma =
-      "fulltext[pdf,txt];metadata[jsonxml];enrichments[teeft,nb]";
-    const missingSemiColon = "fulltext[pdf,txt]metadata[json]";
-    const almostCorrectCategory = "fulltextERROR[pdf];metadata[json]";
+  describe("isFormatSelected", () => {
+    const formatsWithTxt = formats.fulltext.pdf | formats.fulltext.txt;
+    const formatsWithoutTxt = formats.fulltext.pdf;
 
-    expect(Module.parseExtractParams(correct)).toBe(
-      formats.fulltext.pdf |
+    it("detects when a format is selected", () => {
+      expect(
+        Module.isFormatSelected(formatsWithTxt, formats.fulltext.txt),
+      ).toBe(true);
+    });
+
+    it("detects when a format is not selected", () => {
+      expect(
+        Module.isFormatSelected(formatsWithoutTxt, formats.fulltext.txt),
+      ).toBe(false);
+    });
+  });
+
+  describe("isWholeCategorySelected", () => {
+    const metadataFormats =
+      formats.metadata.json | formats.metadata.xml | formats.metadata.mods;
+
+    it("detects when a category is selected", () => {
+      expect(Module.isWholeCategorySelected(metadataFormats, "metadata")).toBe(
+        true,
+      );
+    });
+
+    it("detects when a category is not selected", () => {
+      expect(
+        Module.isWholeCategorySelected(formats.metadata.json, "metadata"),
+      ).toBe(false);
+    });
+
+    it("detects when a category is selected even if formats from other categories are selected", () => {
+      expect(
+        Module.isWholeCategorySelected(
+          metadataFormats | formats.fulltext.pdf,
+          "metadata",
+        ),
+      ).toBe(true);
+    });
+  });
+
+  describe("getWholeCategoryFormat", () => {
+    it("generates the correct format for a category", () => {
+      const fulltextFormats =
+        formats.fulltext.pdf |
+        formats.fulltext.tei |
+        formats.fulltext.txt |
+        formats.fulltext.cleaned |
+        formats.fulltext.zip |
+        formats.fulltext.tiff;
+
+      const metadataFormats =
+        formats.metadata.json | formats.metadata.xml | formats.metadata.mods;
+
+      const enrichmentsFormats =
+        formats.enrichments.multicat |
+        formats.enrichments.nb |
+        formats.enrichments.grobidFulltext |
+        formats.enrichments.refBibs |
+        formats.enrichments.teeft |
+        formats.enrichments.unitex;
+
+      expect(Module.getWholeCategoryFormat("fulltext")).toBe(fulltextFormats);
+      expect(Module.getWholeCategoryFormat("metadata")).toBe(metadataFormats);
+      expect(Module.getWholeCategoryFormat("enrichments")).toBe(
+        enrichmentsFormats,
+      );
+    });
+  });
+
+  describe("buildExtractParamsFromFormats", () => {
+    it("generates the correct extract parameter", () => {
+      const selectedFormats =
+        formats.fulltext.pdf |
         formats.fulltext.txt |
         formats.enrichments.grobidFulltext |
         formats.metadata.json |
         formats.others.annexes |
-        formats.others.covers,
-    );
+        formats.others.covers;
 
-    expect(Module.parseExtractParams(wrongFormat)).toBe(formats.metadata.json);
+      const expectedExtractParams = [
+        /fulltext\[pdf,txt\]/,
+        /metadata\[json\]/,
+        /enrichments\[grobidFulltext\]/,
+        /covers/,
+        /annexes/,
+      ];
 
-    expect(Module.parseExtractParams(wrongCategory)).toBe(formats.fulltext.pdf);
+      const extractParams =
+        Module.buildExtractParamsFromFormats(selectedFormats);
+      expectedExtractParams.forEach((extractParam) => {
+        expect(extractParams).toMatch(extractParam);
+      });
+    });
 
-    expect(Module.parseExtractParams(missingComma)).toBe(
-      formats.fulltext.pdf |
-        formats.fulltext.txt |
-        formats.enrichments.teeft |
-        formats.enrichments.nb,
-    );
+    it("generates an empty string when no formats are selected", () => {
+      expect(Module.buildExtractParamsFromFormats(NO_FORMAT_SELECTED)).toBe("");
+    });
+  });
 
-    expect(Module.parseExtractParams(missingSemiColon)).toBe(
-      formats.fulltext.pdf | formats.fulltext.txt,
-    );
+  describe("parseExtractParams", () => {
+    it("extracts the formats from a correct extract parameter", () => {
+      const correct =
+        "fulltext[pdf,txt];metadata[json];enrichments[grobidFulltext];covers;annexes";
 
-    expect(Module.parseExtractParams(almostCorrectCategory)).toBe(
-      formats.metadata.json,
-    );
+      expect(Module.parseExtractParams(correct)).toBe(
+        formats.fulltext.pdf |
+          formats.fulltext.txt |
+          formats.enrichments.grobidFulltext |
+          formats.metadata.json |
+          formats.others.annexes |
+          formats.others.covers,
+      );
+    });
+
+    it("skips incorrect formats", () => {
+      const wrongFormat = "fulltext[incorrect];metadata[json]";
+
+      expect(Module.parseExtractParams(wrongFormat)).toBe(
+        formats.metadata.json,
+      );
+    });
+
+    it("skips incorrect categories", () => {
+      const wrongCategory = "fulltext[pdf];incorrect[json]";
+      const almostCorrectCategory = "fulltextERROR[pdf];metadata[json]";
+
+      expect(Module.parseExtractParams(wrongCategory)).toBe(
+        formats.fulltext.pdf,
+      );
+      expect(Module.parseExtractParams(almostCorrectCategory)).toBe(
+        formats.metadata.json,
+      );
+    });
+
+    it("skips formats not separated by a comma", () => {
+      const missingComma =
+        "fulltext[pdf,txt];metadata[jsonxml];enrichments[teeft,nb]";
+
+      expect(Module.parseExtractParams(missingComma)).toBe(
+        formats.fulltext.pdf |
+          formats.fulltext.txt |
+          formats.enrichments.teeft |
+          formats.enrichments.nb,
+      );
+    });
+
+    it("skips categories not separated by semicolons", () => {
+      const missingSemiColon = "fulltext[pdf,txt]metadata[json]";
+
+      expect(Module.parseExtractParams(missingSemiColon)).toBe(
+        formats.fulltext.pdf | formats.fulltext.txt,
+      );
+    });
   });
 
   describe("estimateArchiveSize", () => {
